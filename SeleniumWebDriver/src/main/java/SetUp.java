b8e3112e666d321489c1b42540a28fe96d78a1cf
elton098 <123ekene@gmail.com>
2016-12-04 16:20:25 -0500

    selenium setup

---
+++ SeleniumWebDriver/src/main/java/SetUp.java
@@ -0,0 +1,12 @@
+import org.junit.Before;
+
+/**
+ * Created by E on 12/4/2016.
+ */
+public class SetUp {
+
+    @Before
+    public void Setup(){
+
+    }
+}
---
+++ SeleniumWebDriver/src/test/java/Demo.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by E on 12/4/2016.
+ */
+public class Demo {
+
+    @Test
+    public void RunDemo() throws InterruptedException {
+
+        // launch Firefox and direct it to the Base URL
+
+
+
+    }
+}
