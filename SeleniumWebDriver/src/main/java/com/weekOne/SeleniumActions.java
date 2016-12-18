60521cf374914bed7a5494326ad5d1f412893a15
elton098 <123ekene@gmail.com>
2016-12-18 13:38:44 -0500

    Added weekOne Branch

---
+++ SeleniumWebDriver/src/main/java/com/weekOne/SeleniumActions.java
@@ -0,0 +1,7 @@
+package com.weekOne;
+
+/**
+ * Created by E on 12/18/2016.
+ */
+public class SeleniumActions {
+}
---
+++ SeleniumWebDriver/src/test/java/com/weekone_test/SeleniumActions_Test.java
@@ -0,0 +1,7 @@
+package com.weekone_test;
+
+/**
+ * Created by E on 12/18/2016.
+ */
+public class SeleniumActions_Test {
+}
