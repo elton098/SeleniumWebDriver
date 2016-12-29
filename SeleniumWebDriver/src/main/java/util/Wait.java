d32b35747fded33a968b0721efbb772123da15e5
elton098 <123ekene@gmail.com>
2016-12-29 11:13:51 -0500

    added wait class

---
+++ SeleniumWebDriver/src/main/java/util/Wait.java
@@ -0,0 +1,19 @@
+package util;
+
+import Drivers.SelDriver;
+
+/**
+ * Created by E on 12/29/2016.
+ */
+public class Wait extends SelDriver {
+
+
+    //Wait for specified amount of seconds
+    public static void wait(int milliseconds){
+        try {
+            Thread.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
