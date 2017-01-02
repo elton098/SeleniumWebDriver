ff30ce0e18e87f34973dceef8d7d1ae1daa1d55d
KyleAlex1992 <Kyle.jasper92@yahoo.com>
2017-01-01 21:33:36 -0500

    Fix of second week homework as well as construction of all wait methods

--- SeleniumWebDriver/src/main/java/util/Wait.java
+++ SeleniumWebDriver/src/main/java/util/Wait.java
@@ -29,4 +29,45 @@ public class Wait extends SelDriver {
     }
 
 
+    public static void waitForID(WebDriver driver, String id){
+        WebDriverWait wait = new WebDriverWait(driver,30);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(id)));
+
+    }
+
+
+    public static void waitForLinkText(WebDriver driver, String linkText){
+        WebDriverWait wait = new WebDriverWait(driver,30);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText(linkText)));
+    }
+
+
+    public static void waitForClassName(WebDriver driver, String className ){
+        WebDriverWait wait = new WebDriverWait(driver,30);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className(className)));
+    }
+
+
+    public static void waitForName(WebDriver driver, String name){
+        WebDriverWait wait = new WebDriverWait(driver,30);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name(name)));
+    }
+
+
+    public static void waitForXpath(WebDriver driver, String xPath){
+        WebDriverWait wait = new WebDriverWait(driver,30);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xPath)));
+    }
+
+
+    public static void waitForPartialLinkText(WebDriver driver, String partialLinkText) {
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.partialLinkText(partialLinkText)));
+
+    }
+
+    public static void waitForTagName(WebDriver driver, String tagName){
+        WebDriverWait wait = new WebDriverWait(driver,30);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName(tagName)));
+    }
 }
